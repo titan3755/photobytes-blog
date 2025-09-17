ff0a04a3816cd429557bb09433c52f45af30886d
Titan <68808479+titan3755@users.noreply.github.com>
2025-09-17 16:40:49 +0600

    added prisma db (postgres) functionality

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,25 @@
 import type { Metadata } from 'next';
+import prisma from '@/lib/prisma'
 
 export const metadata: Metadata = {
   title: 'Home | PhotoBytes Studios',
   description: 'Welcome to the PhotoBytes Blog, your source for the latest news and updates.',
 };
 
-export default function Home() {
+export default async function Home() {
+  const users = await prisma.user.findMany();
   return (
-    <div className="bg-white min-h-screen w-screen">
-      <main>
-        <h1 className="text-2xl font-bold text-center py-12 text-black text-wrap">Welcome to PhotoBytes Blog</h1>
-      </main>
+    <div className="min-h-screen min-w-screen bg-gray-50 flex flex-col items-center justify-center -mt-16">
+      <h1 className="text-4xl font-bold mb-8 text-black">
+        Superblog
+      </h1>
+      <ol className="list-decimal list-inside text-black">
+        {users.map((user) => (
+          <li key={user.id} className="mb-2 text-black">
+            {user.name}
+          </li>
+        ))}
+      </ol>
     </div>
   )
 }
