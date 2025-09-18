716f02823e80457d2af1c23bfd0c7f0b70829a1f
Titan <68808479+titan3755@users.noreply.github.com>
2025-09-18 20:32:38 +0600

    mods to dev page

--- app/dev/page.tsx
+++ app/dev/page.tsx
@@ -3,6 +3,7 @@ export default function Dev() {
         <>
             <div className="min-h-screen min-w-screen bg-gray-50 flex flex-col items-center justify-center -mt-16">
                 <h1 className="text-black">Developer Page</h1>
+                <h2 className="text-black">Welcome to the Developer Page</h2>
             </div>
         </>
     )
