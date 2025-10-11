6bd3cd42d05b7df14c04d4cadc54d5b4df6fe46c
Titan <68808479+titan3755@users.noreply.github.com>
2025-10-11 22:29:47 +0600

    mini change to home page (placeholder)

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from 'next';
-import prisma from '@/lib/prisma'
+// import prisma from '@/lib/prisma'
 
 export const metadata: Metadata = {
   title: 'Home | PhotoBytes Studios',
@@ -7,19 +7,19 @@ export const metadata: Metadata = {
 };
 
 export default async function Home() {
-  const users = await prisma.user.findMany();
+  // const users = await prisma.user.findMany();
   return (
     <div className="min-h-screen min-w-screen bg-gray-50 flex flex-col items-center justify-center -mt-16">
       <h1 className="text-4xl font-bold mb-8 text-black">
-        Superblog (Photobytes)
+        Welcome to PhotoBytes Studios' Blog!
       </h1>
-      <ol className="list-decimal list-inside text-black">
+      {/* <ol className="list-decimal list-inside text-black">
         {users.map((user: { id: string; email: string }) => (
           <li key={user.id} className="mb-2 text-black">
             {user.email}
           </li>
         ))}
-      </ol>
+      </ol> */}
     </div>
   )
 }
