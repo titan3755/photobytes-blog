c70f78a46b8b123d2efbfd4fc484b4945aa1a006
Titan <68808479+titan3755@users.noreply.github.com>
2025-10-22 14:26:13 +0600

    added new dashboard page

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+// app/dashboard/page.tsx
+export default function Dashboard() {
+  return (
+    <div>
+      <h1>Dashboard Test</h1>
+      <p>If you can see this, the page loaded.</p>
+    </div>
+  );
+}
