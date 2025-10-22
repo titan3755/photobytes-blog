94cbfca55b863a4e2aad6a2386983a053de14ec2
Titan <68808479+titan3755@users.noreply.github.com>
2025-10-22 23:57:53 +0600

    added basic admin page

---
+++ app/admin/page.tsx
@@ -0,0 +1,11 @@
+export default function Admin() {
+    return (
+        <>
+            <div className="min-h-screen min-w-screen bg-gray-50 flex flex-col items-center justify-center -mt-16">
+                <h1 className="text-black">Admin Page</h1>
+                <h2 className="text-black">Welcome to the Admin Page</h2>
+                <h3 className="text-black">Every aspect of the website can be controlled from here</h3>
+            </div>
+        </>
+    )
+}
