583cb21969ab59e4880a89b8a0d1f2a1faf26b93
Titan <68808479+titan3755@users.noreply.github.com>
2025-10-23 00:01:20 +0600

    removed comments

--- middleware.ts
+++ middleware.ts
@@ -1,15 +1,7 @@
-// middleware.ts
-
 import NextAuth from 'next-auth';
-import { authConfig } from './auth.config'; // Import the new lightweight config
-
-// Initialize NextAuth with the lightweight config
+import { authConfig } from './auth.config';
 const { auth } = NextAuth(authConfig);
-
-// Export auth as middleware
 export default auth;
-
-// Your matcher config remains the same
 export const config = {
   matcher: ['/dashboard(.*)', '/profile(.*)'],
 };
